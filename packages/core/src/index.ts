6d39a32fc62d09933e1732a84143c6baba808837
inca <boris@okunskiy.name>
2025-11-22 14:47:34 +0300

    chore: generate proper dts

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,42 +1,7 @@
-import Btn from './Btn.vue';
-import HGroup from './HGroup.vue';
-import InputBase from './InputBase.vue';
-import InputGroup from './InputGroup.vue';
-import InputSelect from './InputSelect.vue';
-import InputText from './InputText.vue';
-import InputTextarea from './InputTextarea.vue';
-import Sizer from './Sizer.vue';
-import Tab from './Tab.vue';
-import TabCap from './TabCap.vue';
-import Toggle from './Toggle.vue';
-import VGroup from './VGroup.vue';
+import * as components from './components/index.js';
 
-export {
-    Btn,
-    HGroup,
-    InputBase,
-    InputGroup,
-    InputSelect,
-    InputText,
-    InputTextarea,
-    Sizer,
-    Tab,
-    TabCap,
-    Toggle,
-    VGroup,
-};
+export * from './components/index.js';
 
 export const coreComponents = {
-    Btn,
-    HGroup,
-    InputBase,
-    InputGroup,
-    InputSelect,
-    InputText,
-    InputTextarea,
-    Sizer,
-    Tab,
-    TabCap,
-    Toggle,
-    VGroup,
+    ...components,
 };
