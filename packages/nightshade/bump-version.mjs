951d8e31e985d9dbce784480d347999d63d8836e
inca <boris@okunskiy.name>
2025-11-22 13:27:41 +0300

    chore: version bump script

--- packages/nightshade/bump-version.mjs
+++
@@ -1,9 +0,0 @@
-import { readFile } from 'node:fs/promises';
-
-const newVersion = process.env.$npm_package_version;
-console.log(`Bumping version to ${newVersion}`);
-
-const pkgJson = await readFile('package.json', 'utf-8');
-const replaced = pkgJson.replace(/"(@nightshadeui\/.*?)": ".*?"/g, `"$1": "${newVersion}"`);
-
-console.log(replaced);
