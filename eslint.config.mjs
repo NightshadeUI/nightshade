d6726010f807d89ea99552b48e159185b5b86c86
inca <boris@okunskiy.name>
2026-04-11 20:58:57 +0300

    chore: add pre-commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,12 @@
 import { sharedConfigs } from '@luminable/eslint-config';
+import { vueConfigs } from '@luminable/eslint-config-vue';
 
 export default [
     ...sharedConfigs,
+    ...vueConfigs,
+    {
+        rules: {
+            'import/no-extraneous-dependencies': 'off'
+        }
+    }
 ];
