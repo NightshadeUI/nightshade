006103b8a1c0d58110672fbcf6e4afb4edd47f0c
inca <boris@okunskiy.name>
2026-04-18 18:26:55 +0300

    2.12.2

---
+++ packages/canvas/src/components/CanvasDebugCoords.vue
@@ -0,0 +1,85 @@
+<template>
+    <VGroup
+        class="CanvasDebugCoords"
+        gap="s">
+        <HGroup class="Row">
+            <strong>page</strong>
+            <span>{{ format(pagePos) }}</span>
+        </HGroup>
+        <HGroup class="Row">
+            <strong>viewport</strong>
+            <span>{{ format(viewportPos) }}</span>
+        </HGroup>
+        <HGroup class="Row">
+            <strong>canvas</strong>
+            <span>{{ format(canvasPos) }}</span>
+        </HGroup>
+        <HGroup class="Row">
+            <strong>local</strong>
+            <span>{{ format(localPos) }}</span>
+        </HGroup>
+        <HGroup class="Row">
+            <strong>zoom</strong>
+            <span>{{ canvas.space.zoom.toFixed(2) }}</span>
+        </HGroup>
+    </VGroup>
+</template>
+
+<script>
+export default {
+
+    props: {
+        canvas: { type: Object, required: true },
+    },
+
+    computed: {
+
+        pagePos() {
+            return this.canvas.inputState.lastMousePos;
+        },
+
+        viewportPos() {
+            return this.canvas.space.pageToViewport(this.page);
+        },
+
+        canvasPos() {
+            return this.canvas.space.pageToCanvas(this.page);
+        },
+
+        localPos() {
+            return this.canvas.space.pageToLocal(this.page);
+        },
+
+    },
+
+    methods: {
+        format(pos) {
+            return `(${pos.x.toFixed(2)}, ${pos.y.toFixed(2)})`;
+        },
+    },
+
+};
+</script>
+
+<style scoped>
+.CanvasDebugCoords {
+    position: absolute;
+    z-index: 10;
+    top: 0;
+    left: 0;
+    margin: var(--sp);
+    padding: var(--sp);
+    pointer-events: none;
+
+    font-family: var(--font-monospace);
+    font-size: var(--font-size-s);
+    border: var(--input-border-size) solid var(--color-base-200);
+    border-radius: var(--border-radius);
+
+    background: var(--color-base-0);
+}
+
+.Row {
+    white-space: nowrap;
+}
+</style>
